a41d153bf099a0cb6eb3d29ee01f302f07b36ffa
Daniel <daniel.nicolas.llanes@gmail.com>
2024-06-28 10:58:35 -0300

    modificado el js para añadir solo un punto en cada operando

--- main.js
+++ main.js
@@ -1,38 +1,63 @@
 //document.addEventListener("DOMContentLoaded", () => {
 
-
+// Se extraen las etiquetas necesarias para manejar sus valores:
 let pantalla = document.getElementById("pantalla_calc");
 let numeros = document.getElementsByClassName("tecla");
 let operaciones = document.getElementsByClassName("operador");
 let tecla_c = document.getElementById("tecla-c");
-let del = document.getElementById("tecla-del");
+let eliminar = document.getElementById("tecla-del");
 let operacion = "";
 let operando_a = "";
 let operando_b = "";
 
+// Se crea un bucle a travez de las teclas para asignarles una función para manejar la interrupcion del click:
 for (let i = 0; i < numeros.length; i++) {
     numeros[i].addEventListener("click", () => {
+        // Si la operación esta vacía escribimos sobre el operando a:
         if (operacion === ""){
-            //if (!(operando_a.includes("."))){
-                operando_a += numeros[i].innerText;
-                pantalla.innerText += numeros[i].innerText;
-            //}
-            
-            //console.log(operando_a)
+            let valor = numeros[i].innerText;
+            // Si el valor ingresado es un simbolo igual, la pantalla muestra lo escrito hasta ahora:
+            if (valor === "="){
+                if (operacion === ""){
+                    pantalla.innerText = operando_a;
+                }
+                pantalla.innerText = operando_a;
+            // Si el valor ingresado es un punto, se crea una bifurcación para manejar el caso:
+            }else if (valor === "."){
+                if (operacion === ""){
+                    if(!(operando_a.includes("."))){
+                        operando_a += valor;
+                        pantalla.innerText += valor;
+                    }
+                }
+            }else{
+                if (operacion === ""){
+                    operando_a += valor;
+                    pantalla.innerText += valor;
+                }
+            }
         }else{
+            // Cuando se comienza a escribir el operando b se reinicia la variable de la pantalla: 
             if (operando_b === ""){
                 pantalla.innerText = "";
             }
-            operando_b += numeros[i].innerText;
-            //console.log(operando_b)
-            pantalla.innerText = operando_b;
+            // De igual manera se crea la variable valor para manejar el caso de que un punto sea escrito:
+            let valor_b = numeros[i].innerText;
+            if (valor_b === "."){
+                if (!(operando_b.includes("."))){
+                    operando_b += valor_b;
+                    pantalla.innerText += valor_b;
+                }
+            }else{
+                operando_b += valor_b;
+                pantalla.innerText = operando_b;
         }
         
          
-    });
+    }});
 }
-//});
 
+// Se le asigna el significado a los simbolos de operación mediante un switch:
 for (let i = 0; i < operaciones.length; i++) { 
     operaciones[i].addEventListener("click", () => {
         switch (operaciones[i].innerText){
@@ -54,7 +79,7 @@ for (let i = 0; i < operaciones.length; i++) {
         console.log(operacion)
 });
 }
-
+//Cuando el botón igual es presiondado, se convierten los operandos a valores flotantes y se realiza la operación necesaria:
 igual.addEventListener("click", () => {
     operando_a = parseFloat(operando_a);
     operando_b = parseFloat(operando_b);
@@ -78,15 +103,15 @@ igual.addEventListener("click", () => {
 
 
 })
-
+// La tecla C de la calculadora reinicia todas las variables:
 tecla_c.addEventListener("click", () => {
     operando_a = "";
     operando_b = "";
     operacion = "";
     pantalla.innerText = "";
 })
-
-del.addEventListener("click", () =>{
+// Cuando la tecla de eliminar sea presionada, eliminara un número del primer o segundo operando, según el caso:
+eliminar.addEventListener("click", () =>{
     if(operacion === ""){
          operando_a = operando_a.slice(0, -1);
          pantalla.innerText = operando_a;
